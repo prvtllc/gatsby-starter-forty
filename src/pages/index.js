a29fb7b028ac9d6d22bf1bf77885b322fd4466e8
Domingo Rivera <djr@infosecusa.com>
2019-03-19 01:45:37 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,43 +31,43 @@ class HomeIndex extends React.Component {
                     <section id="one" className="tiles">
                         <article style={{backgroundImage: `url(${pic01})`}}>
                             <header className="major">
-                                <h3>Aliquam</h3>
-                                <p>Ipsum dolor sit amet</p>
+                                <h3>Internet Defamation</h3>
+                                <p>Including Consultation with attorneys to investigate the identity of Internet actors</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic02})`}}>
                             <header className="major">
-                                <h3>Tempus</h3>
-                                <p>feugiat amet tempus</p>
+                                <h3>Computer Forensics Consulting</h3>
+                                <p>Who says that your forensics consultant can't be both a highly trained technical expert and a successful cyber lawyer?</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic03})`}}>
                             <header className="major">
-                                <h3>Magna</h3>
-                                <p>Lorem etiam nullam</p>
+                                <h3>Intellectual Property</h3>
+                                <p>Including IP protection online and utilizing advanced investigation techniques to discover the identity of infringers</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic04})`}}>
                             <header className="major">
-                                <h3>Ipsum</h3>
-                                <p>Nisl sed aliquam</p>
+                                <h3>Cyber Security/h3>
+                                <p>We are experts in cyber security investigations AND in cyber security law.  We provice this expertise to top trial attorneys</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic05})`}}>
                             <header className="major">
-                                <h3>Consequat</h3>
-                                <p>Ipsum dolor sit amet</p>
+                                <h3>Computer Crimes</h3>
+                                <p>We assist businesses in the forensics investigations to stop cyber crimes.  And we assist cyber crime defense attorneys as expert witnesses.</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
                         <article style={{backgroundImage: `url(${pic06})`}}>
                             <header className="major">
-                                <h3>Etiam</h3>
-                                <p>Feugiat amet tempus</p>
+                                <h3>Domain Name Law</h3>
+                                <p>We assist in uncovering the owner of a domain name and in recovering domain names from cyber squatters - or defending those resisting hostile takeovers of their domains through reverse cyber squatting</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
@@ -75,9 +75,9 @@ class HomeIndex extends React.Component {
                     <section id="two">
                         <div className="inner">
                             <header className="major">
-                                <h2>Massa libero</h2>
+                                <h2>About Domingo J. Rivera</h2>
                             </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <p>A successfule cyber attorney and one of the early "Internet lawyers". Having participated in pioneering cases involving cyber crimes, IP, defamation, and Internet related litigation. Also with a background in cyber security, military cyber experience and admitted in Court as an expert witness in the fields of computer forensics and cyber security.</p>
                             <ul className="actions">
                                 <li><Link to="/landing" className="button next">Get Started</Link></li>
                             </ul>
